c1554e1b3c69f493e2807263580c65d66e62c1a6
DZS1212 <diego.zapatero.sobaco@gregoriofer.com>
2026-02-03 09:15:09 +0100

    V4

--- ProyectoT1/src/Juego_de_los_dados/Juego_de_los_dados.java
+++ ProyectoT1/src/Juego_de_los_dados/Juego_de_los_dados.java
@@ -4,6 +4,7 @@
  */
 package Juego_de_los_dados;
 
+import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -14,126 +15,126 @@ import java.util.Scanner;
 public class Juego_de_los_dados {
 
     public static void main(String[] args) {
+        //aqui declaro todas las variiables
         Random r = new Random();
         Scanner sc = new Scanner(System.in);
         int moneyPlayer = 100; //dinero del jugador
         int moneyBot = 100; //dinero de la maquina
         int banca = 0; //dinero de la banca
+        int roll1; //variables que determinan las tiradas
+        int roll2;
+        long timePlayed; //variable que va a inidicar los segundos transcurridos
         boolean finish = false; //iniciamos esto en false para inidicar que el juego no esta terminado
-        boolean win = false; //iniciamos esta variable en falso por que no se ha ganado
-
-        String game;
-        System.out.println("Reglas del juego:\nTiramos dados para decidir el orden\nAl que le haya tocado tirara los dados");
-        System.out.println("Si te toca un 7 o un 11 ganaras la ronda automaticamente");
-        System.out.println("Si te toca un 2, 3 o 12 perderas la ronda automaticamente");
-        System.out.println("Si te toca algo diferente tiraras una segunda vez");
-        System.out.println("Si la segunda tirada sale igual que la anterior, ganaras la ronda");
-        System.out.println("Si la tirada sale un 7 perderas la ronda");
-        System.out.println("Si sale algo diferente a su anterior tirada o 7, volveras a tirar hasta que toque alguno de esos resultados");
-        System.out.println("Ganar significa ganar 15 euros, perder significa perder 15 euros y mandarles a la banca");
-        System.out.println("Podras jugar todas las veces que quieras, mientras que tu y yo tengamos mas de 15 euros");
-        System.out.println("Quieres jugar contra mi? (S/N)");
+        boolean win = false; //variable que determina si la ronda fue ganada o perdida
+        String game; //variable que inserta el jugador para aceptar o rechazar la partida       
+        System.out.println("\nQuieres jugar contra mi? (S/N)");
         game = sc.nextLine();
+        // esto valida si se introduce una opcion no validaa
         while (game.toUpperCase().charAt(0) != 'N' && game.toUpperCase().charAt(0) != 'S' || game.length() > 1) {
             System.out.println("Opcion no valida \nIntroduzca de nuevo");
-            System.out.println("Quieres jugar contra mi? (S/N)");
+            System.out.println("\nQuieres jugar contra mi? (S/N)");
             game = sc.nextLine();
         }
+        // esto detecta que el jugador no quiere jugar y cierra el programa
+        Date startTime = new Date();
         if (game.toUpperCase().charAt(0) == 'N') {
             System.out.println("Tenga un buen dia :)");
-            return;
 
         } else {
             do {
 
-                System.out.println("Tirada de salida");
-                System.out.printf("Tu dinero %d%nMi dinero %d%nDinero de la banca %d%n", moneyPlayer, moneyBot, banca);
-                System.out.println("Quien saque mas alto empieza!!");
+                System.out.printf("Tu dinero: %d euros%nMi dinero: %d euros%nDinero de la banca: %d euros%n", moneyPlayer, moneyBot, banca);
+                System.out.println("Quien saque mas alto empieza!!\n");
 
                 do {
-                    boolean round = true; //inidica si la ronda sigue siendo valida
-                    int playerDice = r.nextInt(1, 13);//las inicio aqui para que no haya problemas con el bucle
-                    int botDice = r.nextInt(1, 13);
+                    boolean round = true; //inidica si la ronda sigue siendo valida, se inicia como true por que en este punto se juega la primera ronda
+                    //pongo apartir del 2 por que dos dados lo minimo que sale es un 2
+                    int playerDice = r.nextInt(2, 13); //variables que deciden quien juega
+                    int botDice = r.nextInt(2, 13);
                     System.out.printf("Tu tirada fue %d%n", playerDice);
-                    System.out.printf("Mi tirada fue %d%n", botDice);
-                    while (playerDice == botDice) { //uso un while en estaa ocasion para que en caso de 
+                    System.out.printf("Mi tirada fue %d%n%n", botDice);
+                    while (playerDice == botDice) { //uso un while en esta ocasion para que en caso de que no ocurra este suceso, no entre en el bucle
                         System.out.println("Nuestras tiradas fueron iguales, volveremos a tirar");
-                        playerDice = r.nextInt(1, 13);
-                        //dado jugador Tirada de salida
-                        botDice = r.nextInt(1, 13);
-                        //dado Maquina Tirada de salida
+                        playerDice = r.nextInt(2, 13);
+                        botDice = r.nextInt(2, 13);
                         System.out.printf("%nTu tirada fue %d%n", playerDice);
                         System.out.printf("Mi tirada fue %d%n%n", botDice);
                     }
-                    int tirada1; //las inicio aqui para usarlo en ambos lados
-                    int tirada2;
-                    tirada1 = r.nextInt(1, 13); //inicio
+                    roll1 = r.nextInt(2, 13); //inicio
                     if (playerDice > botDice) {
                         System.out.println("Te toca a ti");
-                        System.out.printf("\nResultado de tu primera tirada %d%n", tirada1);
-                        do { //aqui no polgo else if para que revise todas las opciones posibles, si no solo se queda con la primera
-                            tirada2 = r.nextInt(1, 13);
-                            if (tirada1 == 7 || tirada1 == 11 || tirada1 == 2 || tirada1 == 3 || tirada1 == 12 || tirada1 != 7 && tirada2 == 7 || tirada1 == tirada2) {
+                        System.out.printf("\nResultado de tu primera tirada %d%n", roll1);
+                        do {
+                            roll2 = r.nextInt(2, 13);
+                            // todas las posibles condiciones para que la ronda termine y lo que pasa en cada caso
+                            //aqui no polgo else if para que revise todas las opciones posibles, si no solo se queda con la primera
+
+                            if (roll1 == 7 || roll1 == 11 || roll1 == 2 || roll1 == 3 || roll1 == 12 || roll1 != 7 && roll2 == 7 || roll1 == roll2) {
                                 finish = true;
                                 round = false;
-                                if (tirada1 == 7 || tirada1 == 11 || tirada1 == tirada2) {
-                                    win = true;
-                                }
-                                if (tirada1 == 2 || tirada1 == 3 || tirada1 == 12 || tirada1 != 7 && tirada2 == 7) {
-                                    win = false;
-                                }
-                                if (tirada1 != 7 && tirada2 == 7) {
-                                    System.out.printf("Tu tirada para el punto %d%n", tirada2);
-                                    System.out.println("\nSacaste un 7 antes de sacar tu punto");
-                                }
-                                if (tirada1 == tirada2) {
-                                    System.out.printf("Tu tirada para el punto %d%n", tirada2);
-                                    System.out.println("\nTu segunda tirada es igual que la primera");
-                                }
                             }
+                            if (roll1 == 7 || roll1 == 11 || roll1 == roll2) {
+                                win = true;
+                            }
+                            if (roll1 == 2 || roll1 == 3 || roll1 == 12 || roll1 != 7 && roll2 == 7) {
+                                win = false;
+                            }
+                            if (roll1 != 7 && roll2 == 7) {
+                                System.out.printf("Tu tirada para el punto %d%n", roll2);
+                                System.out.println("\nSacaste un 7 antes de sacar tu punto");
+                            }
+                            if (roll1 == roll2) {
+                                System.out.printf("Tu tirada para el punto %d%n", roll2);
+                                System.out.println("\nTu tirada es igual que el punto");
+                            }
+                            System.out.printf("Tu tirada para el punto %d%n", roll2);
                         } while (round == true);
                         if (win == true && finish == true) {
-                            System.out.println("Enorabuena, has ganado");
+                            System.out.println("Enorabuena, has ganado 15 euros");
                             moneyPlayer += 15;
+                            moneyBot -= 15;
                         } else if (win == false && finish == true) {
-                            System.out.println("Que pena has perdido :(");
+                            System.out.println("Que pena has perdido :(\n15 euros para la banca");
                             moneyPlayer -= 15;
                             banca += 15;
                         }
                     } else {
                         System.out.println("Me toca a mi");
-                        System.out.printf("Resultado de mi tirada es %d%n", tirada1);
-                        do { //aqui no polgo else if para que revise todas las opciones posibles, si no solo se queda con la primera
-                            tirada2 = r.nextInt(1, 13);
-                            if (tirada1 == 7 || tirada1 == 11 || tirada1 == 2 || tirada1 == 3 || tirada1 == 12 || tirada1 != 7 && tirada2 == 7 || tirada1 == tirada2) {
+                        System.out.printf("Resultado de mi tirada es %d%n", roll1);
+                        do {
+                            roll2 = r.nextInt(2, 13);
+                            // todas las posibles condiciones para que la ronda termine y lo que pasa en cada caso
+                            //aqui no polgo else if para que revise todas las opciones posibles, si no solo se queda con la primera
+                            if (roll1 == 7 || roll1 == 11 || roll1 == 2 || roll1 == 3 || roll1 == 12 || roll1 != 7 && roll2 == 7 || roll1 == roll2) {
                                 finish = true;
                                 round = false;
-                                if (tirada1 == 7 || tirada1 == 11 || tirada1 == tirada2) {
-                                    win = true;
-                                }
-                                if (tirada1 == 2 || tirada1 == 3 || tirada1 == 12 || tirada1 != 7 && tirada2 == 7) {
-                                    win = false;
-                                }
-                                if (tirada1 != 7 && tirada2 == 7) {
-                                    System.out.printf("Tu tirada para el punto %d%n", tirada2);
-                                    System.out.println("\nSacaste un 7 antes de sacar tu punto");
-                                }
-                                if (tirada1 == tirada2) {
-                                    System.out.printf("Tu tirada para el punto %d%n", tirada2);
-                                    System.out.println("\nTu segunda tirada es igual que la primera");
-                                }
                             }
+                            if (roll1 == 7 || roll1 == 11 || roll1 == roll2) {
+                                win = true;
+                            }
+                            if (roll1 == 2 || roll1 == 3 || roll1 == 12 || roll1 != 7 && roll2 == 7) {
+                                win = false;
+                            }
+                            if (roll1 != 7 && roll2 == 7) {
+                                System.out.printf("Mi tirada para el punto %d%n", roll2);
+                                System.out.println("\nHe sacado un 7 antes de sacar mi punto");
+                            }
+                            if (roll1 == roll2) {
+                                System.out.printf("Mi tirada para el punto %d%n", roll2);
+                                System.out.println("\nMi tirada es igual que el punto");
+                            }
+                            System.out.printf("Mi tirada para el punto %d%n", roll2);
                         } while (round == true);
                         if (win == true && finish == true) {
-                            System.out.println("\nHe ganado");
+                            System.out.println("\nHe ganado\n15 euros para mi");
                             moneyBot += 15;
+                            moneyPlayer -= 15;
                         } else if (win == false && finish == true) {
-                            System.out.println("\nHe perdido");
+                            System.out.println("\nHe perdido\nLa banca se lleva 15 euros mios");
                             moneyBot -= 15;
                             banca += 15;
                         }
                     }
-                    System.out.printf("%nTu dinero %d%nMi dinero %d%nDinero de la banca %d%n", moneyPlayer, moneyBot, banca);
                 } while (finish == false);
                 System.out.println("Quieres volver a jugar? (S/N)");
                 game = sc.nextLine(); //copio toda la comprobacion de vuelta
@@ -142,19 +143,20 @@ public class Juego_de_los_dados {
                     System.out.println("Quieres volver a jugar? (S/N)");
                     game = sc.nextLine();
                 }
-                if (game.toUpperCase().charAt(0) == 'N') {
-                    System.out.println("Gracias por jugar\nTenga un buen dia :)");
-                    return;
-                }
             } while (game.toUpperCase().charAt(0) == 'S' && moneyBot > 15 && moneyPlayer > 15);
+            Date endTime = new Date();
+            timePlayed = (endTime.getTime() - startTime.getTime()) / 1000;
+            System.out.printf("%nNuestra partida ha durado %d segundos %n", timePlayed);
             System.out.println("Cantidad final de la partida");
             System.out.printf("Dinero del Jugador %d%nDinero de la maquina %d%nDinero de la banca %d%n", moneyPlayer, moneyBot, banca);
+            //aqui no pongos else if para que no entren en caso de no querer boluntarieamnete jugar mas 
             if (moneyBot < 15) {
                 System.out.println("No tengo la cantidad minima para seguir jugando, un placer haber jugado");
-            } else if (moneyPlayer < 15) {
+            }
+            if (moneyPlayer < 15) {
                 System.out.println("No tienes suficiente dinero, una pena :(");
             }
-            System.out.println("No se puede continuar jugando\nGracias por jugar");
+            System.out.println("Gracias por jugar\nTenga un buen dia :)");
         }
     }
 }
